d00fb79d9b72a7dfc6dda1166b6a67cc6454022a
Daniel Calbick <daniel.calbick@yale.edu>
2023-10-10 14:44:22 -0400

    Initial commit

---
+++ sync_recently_liked_spotify.sh
@@ -0,0 +1,7 @@
+#! /bin/zsh
+
+python_script_path="/Users/danielcalbick/science/pycharm_projects/spotify/spotify_update_RecentlyLiked.py"
+python_bin="/Users/danielcalbick/science/pycharm_projects/spotify/env/bin/python3"
+$python_bin "$python_script_path"
+
+
